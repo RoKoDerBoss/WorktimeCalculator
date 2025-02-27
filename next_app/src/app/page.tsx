ae4bbea92e2117ddd5fc8d8960d7212f7cf45b17
RoKoDerBoss <52721415+RoKoDerBoss@users.noreply.github.com>
2025-02-27 16:34:52 +0000

    reset Next.js

--- next_app/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-100">
-      <h1 className="text-4xl text-center font-bold text-gray-700">
-        Welcome to the Next.js App Router with Tailwind CSS!
-      </h1>
-    </div>
-  );
-}
---
+++ worktime-calculator/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-gray-700">
+        Worktime Calculator Test
+      </h1>
+    </main>
+  );
+}
